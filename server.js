2c5377bf4faa3a4142a1d4d186caf9c71e4493ac
Pawan Kumar Singh <161737976+codecraftsmanpawan@users.noreply.github.com>
2024-08-25 19:17:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const app = express();
 
 // Use CORS middleware
 app.use(cors({
-  origin: 'https://ims-frontend-six.vercel.app', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'] 
+  origin: '*', // Allow all origins (not recommended for production)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
